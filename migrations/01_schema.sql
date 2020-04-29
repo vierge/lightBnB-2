5c8f93f03265d97a68de90ee7af7efd21eee335a
vierge <marancholia@gmail.com>
2020-04-29 23:15:52 +0000

    first commit. added migrations/01_schema.sql

---
+++ migrations/01_schema.sql
@@ -0,0 +1,45 @@
+-- lightbnb database schema, please be careful!
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY NOT NULL,
+  name VARCHAR(255) NOT NULL,
+  email VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE properties (
+  id SERIAL PRIMARY KEY NOT NULL,
+  owner_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  title VARCHAR(255),
+  description TEXT,
+  thumbnail_photo_url VARCHAR(255),
+  cover_photo_url VARCHAR(255),
+  cost_per_night INTEGER,
+  parking_spaces INTEGER,
+  number_of_bathrooms INTEGER,
+  number_of_bedrooms INTEGER,
+  -- ADDRESS INFO
+  country VARCHAR(255),
+  street VARCHAR(255),
+  city VARCHAR(255),
+  province VARCHAR(255),
+  post_code VARCHAR(255),
+  active BOOLEAN NOT NULL
+);
+
+CREATE TABLE reservations (
+  id SERIAL PRIMARY KEY NOT NULL,
+  start_date DATE,
+  end_date DATE,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
+
+CREATE TABLE property_reviews (
+  id SERIAL PRIMARY KEY NOT NULL,
+  guest_id INTEGER REFERENCES users(id),
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+  rating SMALLINT NOT NULL,
+  message TEXT
+);
